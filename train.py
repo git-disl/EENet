bug fixed for loass calculation

train.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn.parallel
 import torch.optim
 
-from utils import adjust_learning_rate, accuracy, AverageMeter
+from utils.utils import adjust_learning_rate, accuracy, AverageMeter
 
 
 def train(model, train_loader, criterion, optimizer, epoch, args, train_params):
@@ -52,18 +52,22 @@ def train(model, train_loader, criterion, optimizer, epoch, args, train_params):
         if not isinstance(output, list):
             output = [output]
 
-        loss = torch.tensor(0)
+        #the loss variable must be float type as it handles floating point values from the dataset
+        loss = torch.tensor(0.0, dtype=torch.float)
+        print(f"Data type of loss: {loss.dtype}")
         for j in range(len(output)):
-            loss += (j + 1) * criterion(output[j], target_var) / (args.num_exits * (args.num_exits + 1))
-            if epoch > train_params['num_epoch'] * 0.75 and j < len(output) - 1:
-                T = 3
-                alpha_kl = 0.01
-                loss += torch.nn.KLDivLoss()(torch.log_softmax(output[j] / T, dim=-1),  torch.softmax(output[-1] / T, dim=-1)) * alpha_kl * T * T
+          #torch._C._nn.cross_entropy_loss functions accepts a tensor.
+          loss += (j + 1) * criterion(output[j][0][0], target_var) / (args.num_exits * (args.num_exits + 1))
+          if epoch > train_params['num_epoch'] * 0.75 and j < len(output) - 1:
+            T = 3
+            alpha_kl = 0.01
+            loss += torch.nn.KLDivLoss()(torch.log_softmax(output[j] / T, dim=-1),  torch.softmax(output[-1] / T, dim=-1)) * alpha_kl * T * T
 
         losses.update(loss.item(), input.size(0))
 
         for j in range(len(output)):
-            prec1, prec5 = accuracy(output[j].data, target, topk=(1, 5))
+            relevant_output = output[j][0][0].detach()
+            prec1, prec5 = accuracy(relevant_output, target, topk=(1, 5))
             top1[j].update(prec1.item(), input.size(0))
             top5[j].update(prec5.item(), input.size(0))
 